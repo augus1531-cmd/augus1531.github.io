48e6d7d086cc745d58765df08e085220da643dab
augus1531-cmd <augus1531@gmail.com>
2026-06-01 20:15:36 +0700

    music.py

--- music.py
+++ music.py
@@ -1,9 +1,7 @@
 import subprocess
 
-set_media_volume = int(input("volume music = ")
-
 def set_media_volume(level):
-    """level = 0 ถึง 15"""
     subprocess.run(['termux-volume', 'music', str(level)])
-  
-print(set_media_volume)
+
+level = int(input("ใส่ระดับเสียง (0-15): "))
+set_media_volume(level)
